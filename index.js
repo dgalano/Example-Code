4721e6b748babbc72f0c72e8e953c482b8ad3297
dgalano <55563056+dgalano@users.noreply.github.com>
2022-02-15 14:09:08 -1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+// Load module
+const calculator = require('birthday-calculator');
+ 
+// Instantiate
+const date = new Date('1965,01,07');
+const result = calculator.getDayWhenBorn(date);
+console.log(result);
+
+//use the fs function that comes with node
+const fs = require("fs");
+fs.writeFileSync("file1.txt", result);
